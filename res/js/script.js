c58c448ce913d2bebd432a940869c40dd5cd1ae7
Kadi-RiinToomoja <kadiriin.toomoja@gmail.com>
2021-10-23 19:22:15 +0300

    deleted unnecessary code/comments from js

--- res/js/script.js
+++ res/js/script.js
@@ -1,26 +1,10 @@
-// $(function(){
-//     console.log("hello")
-//     $.get("res/json/posts.json", function(posts){
-//         console.log("hello")
-//         for (post of posts){
-//             let div = $('<div class = "post">');
-//             let userId = $('<h2>').text(post.userId);
-//             let title = $('<h1>').text(post.title);
-//             let body = $('<p>').text(post.body);
-//             div.append(userId);
-//             div.append(title);
-//             div.append(body);
-//             $('body').append(div);
-//         }
-//     })
-// });
-
 $(function(){
     $.get("https://private-8275d-wad21posts1.apiary-mock.com/posts", function(posts){
         let divPostContainer = $('<div id="posts_container">');
         for (post of posts){
-            console.log("hello")
-
+            //console.log("hello")
+            
+            //making all the necessary divs
             let divPost = $('<div class = "post">');
             let divHeader = $('<div class = "post_header">');
             let divauthorName = $('<div class = "name">');
@@ -30,7 +14,7 @@ $(function(){
             let divPostText = $('<div class="post_text">');
             let divPostBottom = $('<div class="post_bottom">');
 
-
+            //making all necessay elements
             let iconUrl = post.author.iconurl;
             let name = $('<p>').text(post.author.name);
             let datetime = $('<p>').text(post.datetime);
@@ -39,16 +23,13 @@ $(function(){
             let button = $('<button>');
             let buttonImg =  post.Button.url;
 
-            //button.append(buttonImg);
-            
-            //authorIcon.append(authorIconUrl);
+            //appending all elements to right divs
             divauthorName.append(name);
             divdatetime.append(datetime);
-
             divauthorIcon.append('<img src="' + iconUrl + '" alt="Author">');
             divPicture.append('<img src="' + picture + '" alt="Post image">');
             divPostText.append(postText)
-            //divPostBottom.append('<img src="' + buttonImg + '" alt="Post image">');
+            
             button.append('<img src="' + buttonImg + '" alt="Post image">');
             divPostBottom.append(button)
             
